a60680fed4719757a37189a7e22e86ec7a453c35
geffenhu23-meet <109904196+geffenhu23-meet@users.noreply.github.com>
2022-07-25 09:52:18 +0300

    commit

---
+++ pythonreview.py
@@ -0,0 +1,22 @@
+def create_youtube_videa(title, description):
+	channel = {"title":title, "description":description, "likes":0, "dislikes":0, "comments":{}}
+	
+	return channel
+
+	def like(youtube_video):
+		if "likes" in channel:
+			channel["likes"]= +1
+			return channel
+
+	def dislikes():
+		if "dislikes" in channel:
+			channel["dislikes"]= +1
+			return channel
+
+	def add_comment(youtube_video, username , comment_text):
+		comments.append("comment_text"):
+		return comments
+
+
+
+
