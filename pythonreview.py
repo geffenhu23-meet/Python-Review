a721fccb49e93a21a317edb88d15c7ab8af5b793
geffenhu23-meet <109904196+geffenhu23-meet@users.noreply.github.com>
2022-07-28 11:06:36 +0300

    vv

--- pythonreview.py
+++ pythonreview.py
@@ -3,20 +3,33 @@ def create_youtube_videa(title, description):
 	
 	return channel
 
-	def like(youtube_video):
-		if "likes" in channel:
-			channel["likes"]= +1
-			return channel
+v1 = create_youtube_videa("food", "human need")
 
-	def dislikes():
-		if "dislikes" in channel:
-			channel["dislikes"]= +1
-			return channel
 
-	def add_comment(youtube_video, username , comment_text):
-		comments.append("comment_text"):
-		return comments
+def like(youtube_video):
+	if "likes" in youtube_video:
+		youtube_video["likes"]+=1
+		return youtube_video
+v1 = like(v1)
 
+print(v1)
+
+def dislikes(youtube_video):
+	if "dislikes" in youtube_video:
+		youtube_video["dislikes"]+=1
+		return youtube_video
+v1 = dislikes(v1)
+print(v1)
+
+def add_comment(youtube_video, username , comment_text):
+	comments.append("comment_text")
+	print(comments)
+	return comments
+
+for i in range(495):
+	v1= like(v1)
+
+v1 = print (v1)
 
 
 
